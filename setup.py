f5a63e9fba88c75e2a7aa72307030376dac2137e
SanketPai0518 <111495475+SanketPai0518@users.noreply.github.com>
2024-07-09 22:51:04 +0530

    source repo name changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r',encoding='utf-8') as f:
 __version__="0.0.0"
 REPO_NAME="TextSummarizer"
 AUTHOR_USER_NAME="Sanket Pai"
-SRC_REPO='TextSummarizer'
+SRC_REPO='Textsummarizer'
 Author_email='sanketsushant18@gmail.com'
 
 setuptools.setup(
