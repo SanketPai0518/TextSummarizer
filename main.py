dcd6b1736d48e4172e374ce7004c3ef25aa5ce76
SanketPai0518 <111495475+SanketPai0518@users.noreply.github.com>
2024-07-10 21:55:30 +0530

    made changes

---
+++ main.py
@@ -0,0 +1,3 @@
+from TXT_SUMMARIZER.logging import logger
+
+logger.info("Logging is set up correctly!")
--- src/main.py
+++
@@ -1,3 +0,0 @@
-from src.TXT_SUMMARIZER.logging import logger
-
-logger.info("Logging is set up correctly!")
