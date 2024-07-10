e8127720f1258fd8a96382a247cb410373cfac6e
SanketPai0518 <111495475+SanketPai0518@users.noreply.github.com>
2024-07-10 20:52:53 +0530

    made changes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", 'r',encoding='utf-8') as f:
 __version__="0.0.0"
 REPO_NAME="TextSummarizer"
 AUTHOR_USER_NAME="SanketPai0518"
-SRC_REPO='TextSummarizer'
+SRC_REPO='TXT_SUMMARIZER'
 Author_email='sanketsushant18@gmail.com'
 
 setuptools.setup(
