003e9e30a91ad53ff2329119466aee48e48afa50
SanketPai0518 <111495475+SanketPai0518@users.noreply.github.com>
2024-07-15 18:18:09 +0530

    changes saved

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# TextSummarizer
+# end to end texxt summarization project
+
+# project workflow
+
+1. update config.yaml file
+2. update params.yaml
+3. update enetity
+4. update configuration manager in src config
+5. update components 
+6. update pipeline
+7. update main.py
+8. update app.py
+9. 
--- src/TXT_SUMMARIZER/utils/common.py
+++ src/TXT_SUMMARIZER/utils/common.py
@@ -1,15 +1,15 @@
 import os
-from box.exceptions import BoxValueError
-import yaml
+from box.exceptions import BoxValueError 
+import yaml 
 from TXT_SUMMARIZER.logging.__innit__ import logger
-from ensure import ensure_annotations
+from ensure import ensure_annotations 
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
 
 @ensure_annotations
-def read_yaml (path_to_yaml: Path):
+def read_yaml(path_to_yaml: Path):
     """        I reads yaml file and returns
         ConfigBox:
         Args:
@@ -22,9 +22,9 @@ def read_yaml (path_to_yaml: Path):
         ConfigBox: ConfigBox type
     """
     try:
-        with open(path_to_yamL) as yaml_file:
+        with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load (yaml_file)
-            logger.info(f"yaml file: {path_to_yamL} loaded successfully")
+            logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError ("yaml file is empty")
